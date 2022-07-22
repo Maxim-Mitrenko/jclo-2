fd5261b92d58662bb1e0f73598a80590338e54eb
Maxim-Mitrenko <klevaya.farm@gmail.com>
2022-07-22 16:06:50 +0300

    Add README.md

--- src/main/java/com/example/homework1/repository/UserRepository.java
+++ src/main/java/com/example/homework1/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.homework1.repository;
 import com.example.homework1.authorities.Authorities;
 import org.springframework.stereotype.Repository;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -10,7 +11,11 @@ import java.util.concurrent.ConcurrentHashMap;
 @Repository
 public class UserRepository {
 
-    private Map<String, List<Authorities>> map = new ConcurrentHashMap<>();
+    private final Map<String, List<Authorities>> map = new ConcurrentHashMap<>();
+
+    public UserRepository() {
+        addUserAuthorities("admin", "admin", Arrays.asList(Authorities.READ, Authorities.WRITE, Authorities.DELETE));
+    }
 
     public List<Authorities> getUserAuthorities(String user, String password) {
         return map.get(userAndPasswordFormat(user, password));
